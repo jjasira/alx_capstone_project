a389e1fedc1f6b52652e234b697448ae49d40e90
James Asira <jjasira2016@gmail.com>
2024-03-13 11:06:09 +0300

    added functionality to the admin page

--- Frontend/admin.js
+++ Frontend/admin.js
@@ -2,13 +2,18 @@ const getContacts = async () => {
   const contacts1 = await fetch("http://127.0.0.1:5000/contacts");
   const contactsData = await contacts1.json();
   contactsData.contacts.forEach((contact) => {
-    const contactDetails = document.createElement("div");
-    const contactDetail = document.createElement("p");
-    contactDetail.innerHTML = `${contact.firstName} ${contact.lastName} ${contact.email}`;
-    contactDetails.appendChild(contactDetail);
-    const contactContainer = document.getElementById("contacts-container");
-    contactContainer.appendChild(contactDetails);
-    console.log(contact);
+    const nameRow = document.createElement("tr");
+    nameRow.key = contact.id;
+    const firstNameDetail = document.createElement("td");
+    const lasttNameDetail = document.createElement("td");
+    const emailDetail = document.createElement("td");
+    firstNameDetail.textContent = contact.firstName;
+    lasttNameDetail.textContent = contact.lastName;
+    emailDetail.textContent = contact.email;
+    nameRow.appendChild(firstNameDetail);
+    nameRow.appendChild(lasttNameDetail);
+    nameRow.appendChild(emailDetail);
+    document.getElementById("table-body").appendChild(nameRow);
   });
 };
 
